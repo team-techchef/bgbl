c5781bcbdb16de62451f6649c7b99936f1989abc
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-01-21 19:49:22 -0500

    i dont know why this changed but ok

--- src/pages/events/index.jsx
+++ src/pages/events/index.jsx
@@ -1,49 +1,49 @@
-import { getPageProps } from 'util/api';
-import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import { pageVariants } from 'util/motionVariants';
-import styles from './events.module.scss';
-import CardCta from 'components/cardCta/cardCta';
-import BlockQuote from 'components/blockQuote/blockQuote';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
-import PlaceholderBlock from 'components/placeholderBlock/PlaceholderBlock';
+import { getPageProps } from 'util/api'
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
+import { pageVariants } from 'util/motionVariants'
+import styles from './events.module.scss'
+import CardCta from 'components/cardCta/cardCta'
+import BlockQuote from 'components/blockQuote/blockQuote'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
+import PlaceholderBlock from 'components/placeholderBlock/PlaceholderBlock'
 
 const Events = ({ data }) => {
-  // const [quotes, setQuotes] = useState([null]);
-  // const [topQuote, setTopQuote] = useState([null]);
-
   const quotes = data?.reviews?.reviews?.map((v, i) => {
-    // if (i === 0) setTopQuote(<BlockQuote key={`bp${i}`} {...v} />);
-    // else setQuotes([quotes, <BlockQuote key={`bp${i}`} {...v} />]);
-    return <BlockQuote key={`bp${i}`} {...v} />;
-  });
+    return <BlockQuote key={`bp${i}`} {...v} />
+  })
 
   return (
     <motion.div
       className={styles.layout}
-      initial="initial"
-      animate="enter"
-      exit="exit"
+      initial='initial'
+      animate='enter'
+      exit='exit'
       variants={pageVariants}
     >
-      <h1 className="header-1">{data?.header}</h1>
+      <h1 className='header-1'>{data?.header}</h1>
       {quotes[0] && quotes.shift()}
       <section>
-        <h2 className="header-two">{data?.upComingEvent?.title}</h2>
+        <h2 className='header-two'>{data?.upComingEvent?.title}</h2>
 
         {data?.upComingEvent?.events?.length > 0 ? (
           <div className={styles['card-container']}>
             {data?.upComingEvent?.events?.map((obj, i) => {
               return (
-                <CardCta key={`upComingEvent${i}`} image={obj?.image}>
+                <CardCta
+                  key={i}
+                  link={obj?.link}
+                  key={`upComingEvent${i}`}
+                  image={obj?.image}
+                >
                   <div>
                     <strong>{obj?.title}</strong>
                   </div>
                   <time>{obj?.date}</time> <span>{obj?.location}</span>
                   <div className={styles['card-btn']}>Attend this Event</div>
                 </CardCta>
-              );
+              )
             })}
           </div>
         ) : (
@@ -51,7 +51,7 @@ const Events = ({ data }) => {
         )}
       </section>
       <section>
-        <h2 className="header-two">{data?.pastEvent?.title}</h2>
+        <h2 className='header-two'>{data?.pastEvent?.title}</h2>
         {data?.pastEvent?.events?.length > 0 ? (
           <div className={styles['carousel-container']}>
             <Carousel showArrows={true}>
@@ -63,9 +63,9 @@ const Events = ({ data }) => {
                       alt={obj.image.alt}
                       className={styles.carousel_image}
                     />
-                    <p className="legend">{obj.title}</p>
+                    <p className='legend'>{obj.title}</p>
                   </div>
-                );
+                )
                 // return (
                 //   <CardCta key={`pastEvents${i}`} image={obj?.image}>
                 //     <div>
@@ -83,7 +83,7 @@ const Events = ({ data }) => {
         )}
       </section>
       <section>
-        <h2 className="header-two">{data?.reviews?.title}</h2>
+        <h2 className='header-two'>{data?.reviews?.title}</h2>
         <div className={styles['block-section']}>
           {quotes}
           {/* {data?.reviews?.reviews?.map((v, i) => {
@@ -93,9 +93,9 @@ const Events = ({ data }) => {
         </div>
       </section>
     </motion.div>
-  );
-};
+  )
+}
 
-export const getStaticProps = async () => getPageProps('events');
+export const getStaticProps = async () => getPageProps('events')
 
-export default Events;
+export default Events
