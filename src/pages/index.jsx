38933404bc757c5318fcc1083bf9b5b61cac0645
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-03-26 02:47:33 -0400

    reorganized layout and removed unnecessary garbage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,28 @@
-import styles from './index.module.scss';
-import { getPageProps } from 'util/api';
-import { motion } from 'framer-motion';
-import { pageVariants } from 'util/motionVariants';
-import Accordion from 'components/accordion/accordion';
-import Panel from 'components/Panel/Panel';
-import BlockQuote from 'components/blockQuote/blockQuote';
-import CtaBtn from 'components/cta-btn/ctaBtn';
-import CardCta from 'components/cardCta/cardCta';
-import GroupLayout from 'components/groupLayout/groupLayout';
-import Link from 'next/link';
-import NBWD from 'components/NBWD/NBWD';
-import AccNotice from 'components/accNotice/AccNotice';
+import styles from './index.module.scss'
+import { getPageProps } from 'util/api'
+import { motion } from 'framer-motion'
+import { pageVariants } from 'util/motionVariants'
+import Accordion from 'components/accordion/accordion'
+import Panel from 'components/Panel/Panel'
+import BlockQuote from 'components/blockQuote/blockQuote'
+import CardCta from 'components/cardCta/cardCta'
+import GroupLayout from 'components/groupLayout/groupLayout'
+import Link from 'next/link'
+import NBWD from 'components/NBWD/NBWD'
+import Newsletter from 'components/newsletter/Newsletter'
 
 const Home = ({ data }) => {
-  const { mainHeader, drawer, blockQuote, callToActionCards = [] } = data;
+  const { mainHeader, drawer, blockQuote, callToActionCards = [] } = data
   return (
     <motion.div
       className={styles.layout}
-      initial="initial"
-      animate="enter"
-      exit="exit"
+      initial='initial'
+      animate='enter'
+      exit='exit'
       variants={pageVariants}
     >
-      <h1 className="header-1">{mainHeader}</h1>
-      <BlockQuote {...blockQuote} />
-      <CtaBtn link={blockQuote?.link} text={blockQuote?.cta_text} />
-
+      <h1 className='header-1'>{mainHeader}</h1>
+      <Newsletter />
       <Accordion>
         {drawer.map((obj, i) => {
           return (
@@ -39,38 +36,35 @@ const Home = ({ data }) => {
                         title={bk.title}
                         link={bk.link}
                         src={bk.img}
-                        // alt={bk?.alt}
                       />
-                    );
+                    )
                   })}
                 </div>
               ) : null}
             </Panel>
-          );
+          )
         })}
       </Accordion>
       <GroupLayout>
         {callToActionCards.map(({ image, caption, link }, i) => {
-          const relativePath = /^\//.test(link);
-          return relativePath ? (
-            <Link href={link} key={`card${i}`} passHref>
-              <CardCta image={image} text={caption} />
-            </Link>
-          ) : (
-            <CardCta
-              key={`card${i}`}
-              href={link}
-              image={image}
-              text={caption}
-            />
-          );
+          return (
+            <a href={link} key={`card${i}`} passHref>
+              <CardCta
+                key={`card${i}`}
+                href={link}
+                image={image}
+                text={caption}
+              />
+            </a>
+          )
         })}
       </GroupLayout>
+      <BlockQuote {...blockQuote} />
     </motion.div>
-  );
-};
+  )
+}
 
 export const getStaticProps = async ({ preview, previewData }) =>
-  getPageProps('index', preview, previewData);
+  getPageProps('index', preview, previewData)
 
-export default Home;
+export default Home
