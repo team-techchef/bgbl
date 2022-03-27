d519b1ae827fc86a6f577c692c75663da0a67983
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-03-27 01:41:02 -0400

    make badgegroup component respond better to links and sizes

--- src/components/badgeGroup/badgeGroup.jsx
+++ src/components/badgeGroup/badgeGroup.jsx
@@ -1,11 +1,20 @@
-import styles from './badgeGroup.module.scss';
+import styles from './badgeGroup.module.scss'
 
 const BadgeGroup = ({ badges }) => (
   <div className={styles.container}>
-    {badges.map((v) => (
-      <div className={styles.badge}>{v}</div>
-    ))}
+    {badges.map(({ text, link }) => {
+      if (!link) {
+        return <div className={styles.badge}>{text}</div>
+      }
+      return (
+        <div className={styles.badge}>
+          <a target='_blank' href={link}>
+            {text}
+          </a>
+        </div>
+      )
+    })}
   </div>
-);
+)
 
-export default BadgeGroup;
+export default BadgeGroup
