1347181f93e639fc364c64ba21937a029553716b
Sean Reichel <sean@Seans-MacBook-Pro.local>
2022-04-04 21:03:55 -0400

    reworked layout, much much better

--- src/pages/publications/index.jsx
+++ src/pages/publications/index.jsx
@@ -14,70 +14,72 @@ const Publications = ({ data }) => {
       exit='exit'
       variants={pageVariants}
     >
-      <BlockQuote {...data.blockQuote} />
-      <section className={styles.read}>
-        <h2 className='header-two'>{data.bookReviews.title}</h2>
-        {data.bookReviews.publications?.length > 0 ? (
-          <div className={styles.read_pubs}>
-            {data.bookReviews.publications.map((pub) => {
-              return (
-                <Publication
-                  title={pub.title}
-                  src={pub.image.src}
-                  alt={pub.image.alt}
-                  link={pub.link}
-                  linkText={pub.linkText}
-                  type={pub.type}
-                />
-              )
-            })}
-          </div>
-        ) : (
-          <></>
-        )}
-      </section>
-      <section className={styles.download}>
-        <h2 className='header-two'>{data.essaysArticles.title}</h2>
-        {data.essaysArticles.publications?.length > 0 ? (
-          <div className={styles.download_pubs}>
-            {data.essaysArticles.publications.map((pub) => {
-              return (
-                <Publication
-                  title={pub.title}
-                  src={pub.image.src}
-                  alt={pub.image.alt}
-                  link={pub.link}
-                  linkText={pub.linkText}
-                  type={pub.type}
-                />
-              )
-            })}
-          </div>
-        ) : (
-          <></>
-        )}
-      </section>
-      <section className={styles.fiction}>
-        <h2 className='header-two'>{data.fiction.title}</h2>
-        {data.fiction.publications?.length > 0 ? (
-          <div className={styles.fiction_pubs}>
-            {data.fiction.publications.map((pub) => {
-              return (
-                <Publication
-                  title={pub.title}
-                  src={pub.image.src}
-                  alt={pub.image.alt}
-                  link={pub.link}
-                  linkText={pub.linkText}
-                  type={pub.type}
-                />
-              )
-            })}
-          </div>
-        ) : (
-          <></>
-        )}
-      </section>
+      <div className={styles.wrapper}>
+        <BlockQuote {...data.blockQuote} />
+        <section className={styles.read}>
+          <h2 className='header-two'>{data.bookReviews.title}</h2>
+          {data.bookReviews.publications?.length > 0 ? (
+            <div className={styles.read_pubs}>
+              {data.bookReviews.publications.map((pub) => {
+                return (
+                  <Publication
+                    title={pub.title}
+                    src={pub.image.src}
+                    alt={pub.image.alt}
+                    link={pub.link}
+                    linkText={pub.linkText}
+                    type={pub.type}
+                  />
+                )
+              })}
+            </div>
+          ) : (
+            <></>
+          )}
+        </section>
+        <section className={styles.download}>
+          <h2 className='header-two'>{data.essaysArticles.title}</h2>
+          {data.essaysArticles.publications?.length > 0 ? (
+            <div className={styles.download_pubs}>
+              {data.essaysArticles.publications.map((pub) => {
+                return (
+                  <Publication
+                    title={pub.title}
+                    src={pub.image.src}
+                    alt={pub.image.alt}
+                    link={pub.link}
+                    linkText={pub.linkText}
+                    type={pub.type}
+                  />
+                )
+              })}
+            </div>
+          ) : (
+            <></>
+          )}
+        </section>
+        <section className={styles.fiction}>
+          <h2 className='header-two'>{data.fiction.title}</h2>
+          {data.fiction.publications?.length > 0 ? (
+            <div className={styles.fiction_pubs}>
+              {data.fiction.publications.map((pub) => {
+                return (
+                  <Publication
+                    title={pub.title}
+                    src={pub.image.src}
+                    alt={pub.image.alt}
+                    link={pub.link}
+                    linkText={pub.linkText}
+                    type={pub.type}
+                  />
+                )
+              })}
+            </div>
+          ) : (
+            <></>
+          )}
+        </section>
+      </div>
       <p className={styles.subheader}>
         <em>{data.subheader}</em>
       </p>
